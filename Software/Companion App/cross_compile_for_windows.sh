6a1f21f35da485501b3dbfb24d108c9c3c4d35ee
Louis Dalibard <louis.dalibard@gmail.com>
2023-09-18 23:45:12 +0200

    fix: windows build

--- Software/Companion App/cross_compile_for_windows.sh
+++ Software/Companion App/cross_compile_for_windows.sh
@@ -3,4 +3,4 @@ pacman -S mingw-w64-x86_64-pkg-config
 pacman -S mingw-w64-x86_64-portaudio
 pacman -S mingw-w64-x86_64-go
 export GOROOT=/mingw64/lib/go
-GOOS=windows CGO_ENABLED=1 CC=mingw-w64-x86_64-gcc go build
+GOOS=windows CGO_ENABLED=1 CC=gcc go build
